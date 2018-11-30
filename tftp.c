7a0c0f608d8965200fb6fa257c19b0afa91323e7
Mario Virdis <virdis.mario97@gmail.com>
2018-11-30 16:33:13 +0100

    Aggiunta inizializz server e rimossa dipendenza da pthread

---
+++ tftp.c
@@ -0,0 +1,8 @@
+void handle_req(int ssock) {
+
+	char opcode[2];
+	socklen_t len;
+	struct sockaddr_in client_addr;
+	int quant;
+	
+} 
--- tftp_server.c
+++ tftp_server.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
+#include <string.h>
 
 #include "tftp.h"
 
 char* directory;
 int port;
+int server_socket;
+int exit_status;
+struct sockaddr_in client_addr;
+struct sockaddr_in server_addr;
 
 int main(int argc, char** argv) {
 	// Controllo parametri server
@@ -27,6 +31,22 @@ int main(int argc, char** argv) {
 		printf("TFTP Directory: %s\n", directory);
 		printf("TFTP Port: %d\n", port);
 	#endif
+
+	memset(&server_addr, 0, sizeof(struct sockaddr_in));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port);
+	inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+
+	// Socket UDP del server
+	server_socket = socket(AF_INET, SOCK_DGRAM, 0);
+
+	exit_status = bind(server_socket, (struct sockaddr*) &server_addr, sizeof(struct sockaddr_in));
+	if (exit_status == -1) {
+		printf("Non è stato possibile assegnare la porta %d al server.\n", port);
+		printf("Se si utilizza la porta di default eseguire il server da root.\n");
+		close(server_socket);
+		return 0;
+	}
 
 	return 0;
 }
