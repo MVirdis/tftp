1dac0099aa6ff27c41dfdb088d4466b97bf04035
Mario Virdis <virdis.mario97@gmail.com>
2018-12-01 17:32:54 +0100

    Correzione endianess message_type modulo tftp

--- tftp.c
+++ tftp.c
@@ -1,10 +1,11 @@
 #include "tftp.h"
 
 #include <string.h>
+#include <arpa/inet.h>
 
 int get_message_type(char* message) {
 	short opcode = 0;
 	// Prelevo l'opcode
 	memcpy(&opcode, message, 2);
-	return (int)opcode;
+	return (int)ntohs(opcode);
 }
