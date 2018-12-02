f58ec290f1dabd14183c163526849a737375d29b
Mario Virdis <virdis.mario97@gmail.com>
2018-12-02 16:30:59 +0100

    Aggiunte funzioni per creare e rimuovere transfer

--- transfer.c
+++ transfer.c
@@ -2,6 +2,8 @@
 
 #include <pthread.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <string.h>
 
 void init_transfer_list(transfer_list_t* list) {
 	*list = NULL;
@@ -27,4 +29,31 @@ void deallocate(struct transfer* transfer) {
 	if (transfer->filepath)
 		free(transfer->filepath);
 	free(transfer);
+}
+
+void remove_transfer(transfer_list_t* list, struct transfer* target) {
+	struct transfer* it,* prev;
+	if(*list == NULL) return;
+	if(*list == target) {
+		*list = (*list)->next;
+		deallocate(target);
+		return;
+	}
+	for(it = *list; it && it!=target; it = it->next)
+		prev = it;
+	if(it == NULL) return;
+	prev->next = it->next;
+	deallocate(it);
+}
+
+struct transfer* create_transfer(int id, struct sockaddr* addr, char* filepath) {
+	struct transfer* new_transfer = malloc(sizeof(struct transfer));
+	new_transfer->id = id;
+	new_transfer->addr = malloc(sizeof(struct sockaddr));
+	memcpy(new_transfer->addr, addr, sizeof(struct sockaddr));
+	pthread_cond_init(&new_transfer->acked, NULL);
+	pthread_mutex_init(&new_transfer->mutex, NULL);
+	strcpy(new_transfer->filepath, filepath);
+	new_transfer->next = NULL;
+	return new_transfer;
 }
--- transfer.h
+++ transfer.h
@@ -12,5 +12,7 @@ struct transfer {
 typedef struct transfer* transfer_list_t;
 
 void init_transfer_list(transfer_list_t* list);
+struct transfer* create_transfer(int id, struct sockaddr* addr, char* filepath);
 int add(transfer_list_t* list, struct transfer* new_transfer);
+void remove_transfer(transfer_list_t* list, struct transfer* target);
 void deallocate(struct transfer* transfer);
