896f3398456d96c171aad0fc807bd4e6fe4b4f64
Mario Virdis <virdis.mario97@gmail.com>
2018-12-02 18:06:51 +0100

    Aggiunte due modalità al get_filechunk

--- file_utils.c
+++ file_utils.c
@@ -9,12 +9,17 @@ int get_file_size(char* filepath) {
 	return info.st_size;
 }
 
-int get_file_chunk(char* buff, char* filepath, int offset, int size) {
+int get_file_chunk(char* buff, char* filepath, int offset, int size, int mode) {
 	FILE* stream;
+	int i;
 	if (filepath == NULL || buff == NULL) return -1;
 	if ((stream = fopen(filepath, "r")) == NULL) return -1;
 	fseek(stream, offset, SEEK_SET);
-	fread(buff, size, 1, stream);
+	if (mode == BIN)
+		fread(buff, size, 1, stream);
+	else if (mode == TEXT)
+		for(i=0; i<size; ++i)
+			buff[i] = (char) fgetc(stream);
 	fclose(stream);
 	return 0;
 }
--- file_utils.h
+++ file_utils.h
@@ -1,2 +1,5 @@
+#define BIN 0
+#define TEXT 1
+
 int get_file_size(char* filepath);
-int get_file_chunk(char* buff, char* filepath, int offset, int size);
+int get_file_chunk(char* buff, char* filepath, int offset, int size, int mode);
