7b118f6c7a159f73baa6423c5e06a78f564c820a
Mario Virdis <virdis.mario97@gmail.com>
2018-12-02 18:07:35 +0100

    Correzione errore in creazione transfer e aggiunta funzione di stampa lista

--- transfer.c
+++ transfer.c
@@ -2,7 +2,9 @@
 
 #include <pthread.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <string.h>
 
 void init_transfer_list(transfer_list_t* list) {
@@ -46,14 +48,31 @@ void remove_transfer(transfer_list_t* list, struct transfer* target) {
 	deallocate(it);
 }
 
-struct transfer* create_transfer(int id, struct sockaddr* addr, char* filepath) {
+struct transfer* create_transfer(int id, struct sockaddr* addr, char* filepath, int mode) {
 	struct transfer* new_transfer = malloc(sizeof(struct transfer));
 	new_transfer->id = id;
 	new_transfer->addr = malloc(sizeof(struct sockaddr));
 	memcpy(new_transfer->addr, addr, sizeof(struct sockaddr));
 	pthread_cond_init(&new_transfer->acked, NULL);
 	pthread_mutex_init(&new_transfer->mutex, NULL);
+	new_transfer->filepath = malloc(strlen(filepath) + 1);
 	strcpy(new_transfer->filepath, filepath);
+	new_transfer->filemode = mode;
 	new_transfer->next = NULL;
 	return new_transfer;
+}
+
+void print_transfer_list(transfer_list_t list) {
+	char paddr[INET_ADDRSTRLEN];
+	struct sockaddr_in* addrp;
+	for(; list; list=list->next) {
+		printf("{\n");
+		printf("\tId: %d\n", list->id);
+		addrp = (struct sockaddr_in*) list->addr;
+		inet_ntop(AF_INET, &addrp->sin_addr, paddr, INET_ADDRSTRLEN);
+		printf("\tAddr: %s:%d\n", paddr, ntohs(addrp->sin_port));
+		printf("\tFilepath: %s\n", list->filepath);
+		printf("\tFilemode: %d\n", list->filemode);
+		printf("}\n");
+	}
 }
--- transfer.h
+++ transfer.h
@@ -4,6 +4,7 @@ struct transfer {
 	int id;
 	struct sockaddr* addr;
 	char* filepath;
+	int filemode;
 	pthread_cond_t acked;
 	pthread_mutex_t mutex;
 	struct transfer* next;
@@ -12,7 +13,8 @@ struct transfer {
 typedef struct transfer* transfer_list_t;
 
 void init_transfer_list(transfer_list_t* list);
-struct transfer* create_transfer(int id, struct sockaddr* addr, char* filepath);
+struct transfer* create_transfer(int id, struct sockaddr* addr, char* filepath, int mode);
 int add(transfer_list_t* list, struct transfer* new_transfer);
 void remove_transfer(transfer_list_t* list, struct transfer* target);
 void deallocate(struct transfer* transfer);
+void print_transfer_list(transfer_list_t list);
