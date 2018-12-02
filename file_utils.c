fc9c069b52c7a4c400c2311b9879e00ffc3d9b3d
Mario Virdis <virdis.mario97@gmail.com>
2018-12-02 13:56:21 +0100

    Aggiunte funzionalità a modulo file_utils

--- file_utils.c
+++ file_utils.c
@@ -1,6 +1,20 @@
 #include "file_utils.h"
 
 #include <stdio.h>
+#include <sys/stat.h>
 
 int get_file_size(char* filepath) {
+	struct stat info;
+	stat(filepath, &info);
+	return info.st_size;
+}
+
+int get_file_chunk(char* buff, char* filepath, int offset, int size) {
+	FILE* stream;
+	if (filepath == NULL || buff == NULL) return -1;
+	if ((stream = fopen(filepath, "r")) == NULL) return -1;
+	fseek(stream, offset, SEEK_SET);
+	fread(buff, size, 1, stream);
+	fclose(stream);
+	return 0;
 }
--- file_utils.h
+++ file_utils.h
@@ -1,1 +1,2 @@
 int get_file_size(char* filepath);
+int get_file_chunk(char* buff, char* filepath, int offset, int size);
