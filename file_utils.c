d22a4bb447e10f021112a02a1fa34c9ab499eece
Mario Virdis <virdis.mario97@gmail.com>
2018-12-08 16:45:27 +0100

    Rimozione funzione modulo file_utils non utilizzata

--- file_utils.c
+++ file_utils.c
@@ -25,27 +25,6 @@ int get_file_chunk(char* buff, char* filepath, int offset, int size, int mode) {
 	return 0;
 }
 
-int set_file_chunk(char* data, char* filepath, int offset, int size, int mode) {
-	FILE* stream;
-	int i;
-	if (filepath == NULL || data == NULL) return -1;
-	if (offset < 0) return -1;
-	// Se devo scrivere il primo blocco sovrascrivo l'eventuale file se presente
-	if (offset == 0)
-		stream = fopen(filepath, "w");
-	else
-		stream = fopen(filepath, "a");
-	if (stream == NULL) return -1;
-	fseek(stream, offset, SEEK_SET);
-	if (mode == BIN)
-		fwrite(data, size, 1, stream);
-	else if (mode == TEXT)
-		for(i=0; i<size; ++i)
-			fprintf(stream, "%c", data[i]);
-	fclose(stream);
-	return 0;
-}
-
 int append_file_chunk(char* data, char* filepath, int size, int mode) {
 	FILE* stream;
 	int i;
